9db786d7ab02a574637cbe72e18a42fab9d5e7c9
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2022-10-19 09:38:12 -0700

    MUI Static Slider

--- src/component/js/GraphDisplay.js
+++ src/component/js/GraphDisplay.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import Slider from '@mui/material/Slider';
 import "../css/GraphDisplay.css";
 
 export default class GraphDisplay extends React.Component {
     render() {
         return (
-            <div className="display">
+            <div className="display-container">
+                <div className="display">
 
+                </div>
+                <div className="slider-container">
+                    <Slider defaultValue={30} step={10} marks min={10} max={110}/>
+                </div>
             </div>
         );
     }
