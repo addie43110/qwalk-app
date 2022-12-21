9399b65507f4c3096df372f859f6ea4c887746fb
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2022-12-21 13:38:22 -0800

    Modified input checking UseEffect(), changed TextInput -> NumberInput, and added in css.modules

--- src/component/js/Options.js
+++ src/component/js/Options.js
@@ -1,73 +1,80 @@
 import React, {useEffect, useState} from "react";
-
 import Checkbox from "./Checkbox";
-import TextInput from "./TextInput";
+import NumberInput from "./NumberInput";
+import classes from "../css/Options.module.css";
 
 const LINE = "line";
 const WIDTH = "width";
 const CUBE = "cube";
 
 const Options = () => {
-    const [type, setType] = useState(LINE);
+    const [type, setType] = useState(CUBE);
     const [magnitude, setMagnitude] = useState(null);
     const [cumulativeProbability, setCumulativeProbability] = useState(false);
     const [steps, setSteps] = useState(null);
 
     useEffect(() => {
         try {
-            if (magnitude == null || steps == null) {
+            if (magnitude === null || steps === null) {
                 return;
             }
 
-            // TODO: Handle err message duplication
-            setMagnitude(parseInt(magnitude));
-            setSteps(parseInt(steps));
-            if (magnitude < 1 || steps < 0 || steps > 20) {
-                throw "Invalid parameter(s) entered. Magnitude must be an integer greater than or equal to 1 \
-                    and steps must be a positive integer no larger than 20.";
-            } 
-
-            if (type === LINE && !(magnitude && !(magnitude & (magnitude - 1)) === 0)) {
-                throw "Invalid magnitude entered. Magnitude must be a power of 2.";
+            if (!Number.isInteger(magnitude) || !Number.isInteger(steps) || magnitude < 1 || steps < 0 || steps > 20) {
+                throw new Error("Invalid parameter(s) entered. Magnitude must be an integer greater than or equal to 1 \
+                    and steps must be a positive integer no larger than 20.");
             }
 
-            let magnitude_squared = Math.pow(magnitude, 2);
-            if (type === WIDTH && !(magnitude_squared && !(magnitude_squared & (magnitude_squared - 1)) === 0)) {
-                throw "Invalid magnitude entered. Squared magnitude must be a power of 2.";
+            if (type === LINE && !isAPowerOfTwo(magnitude)) {
+                throw new Error("Invalid magnitude entered. Magnitude must be a power of 2.");
             }
 
-            let magnitude_cubed = Math.pow(magnitude, 3);
-            if (type === CUBE && !(magnitude_cubed && !(magnitude_cubed & (magnitude_cubed - 1)) === 0)) {
-                throw "Invalid magnitude entered. Cubed magnitude must be a power of 2.";
+            let magnitudeSquared = Math.pow(magnitude, 2);
+            if (type === WIDTH && !isAPowerOfTwo(magnitudeSquared)) {
+                throw new Error("Invalid magnitude entered. Squared magnitude must be a power of 2.");
             }
 
+            let magnitudeCubed = Math.pow(magnitude, 3);
+            if (type === CUBE && !isAPowerOfTwo(magnitudeCubed)) {
+                throw new Error("Invalid magnitude entered. Cubed magnitude must be a power of 2.");
+            }
+
+            setMagnitude(magnitude);
+            setSteps(steps);
         } catch (e) {
-            console.error(e)
+            console.error(`${e.message}`)
         }
-    }, [type, magnitude, cumulativeProbability, steps]);
+    }, [type, magnitude, steps]);
 
+    const isAPowerOfTwo = (value) => {
+        return ((value & (value-1)) === 0);
+    }
 
     return (
-        <div className="options-container">
+        <div className={classes["options-container"]}>
             <h1>Options</h1>
             <h2>Type</h2>
-            <div className="type-container">
+            <div className={classes["type-container"]}>
                 <Checkbox label="line"/>
                 <Checkbox label="grid"/>
                 <Checkbox label="cube"/>
             </div>
             <h2>Dimensions</h2>
-            {/* TODO: Convert to css modules */}
-            <div className="dimensions-container">
-                <TextInput label="Magnitude" onChangeFunction={event => setMagnitude(event.target.value)}/>
+            <div className={classes["dimensions-container"]}>
+                <NumberInput 
+                    label="Magnitude" 
+                    onBlurEvent={event => setMagnitude(event.target.value.length > 0? Number(event.target.value): null)}
+                />
             </div>
             <h2>Cumulative probability?</h2>
-            <div className="cumulative-container">
-                <Checkbox label="Y/N" onChangeFunction={event => setCumulativeProbability(event.target.value)}/>
+            <div className={classes["cumulative-container"]}>
+                <Checkbox label="Y/N"/>
             </div>
             <h2>Number of steps</h2>
-            <div className="steps-container">
-                <TextInput label="step(s)" onChangeFunction={event => setSteps(event.target.value)}/>
+            <div className={classes["steps-container"]}>
+                <NumberInput 
+                    label="Step(s)" 
+                    onBlurEvent={event => setSteps(event.target.value.length > 0? Number(event.target.value): null)}
+                />
             </div>
         </div>
     );
