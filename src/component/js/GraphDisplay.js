5e9c6457095921ad22e4fe328a7ce5b1f3d693ed
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2022-12-21 17:45:25 -0800

    Created separate StepSlider component; added logic for  null, 0, and steps >= 1; and modified styling

--- src/component/js/GraphDisplay.js
+++ src/component/js/GraphDisplay.js
@@ -1,10 +1,6 @@
 import React from "react";
-import Slider from '@mui/material/Slider';
 import classes from "../css/GraphDisplay.module.css"
-
-const SLIDER_NODE_STEP_SIZE = 1;
-const DEFAULT_SLIDER_VALUE = 1;
-const MIN_SLIDER_NODE_COUNT = 1;
+import StepSlider from "./StepSlider";
 
 const GraphDisplay=(props)=> {
     const {steps} = props;
@@ -15,14 +11,7 @@ const GraphDisplay=(props)=> {
 
             </div>
             <div className={classes["slider-container"]}>
-                <Slider 
-                    defaultValue={DEFAULT_SLIDER_VALUE} 
-                    step={SLIDER_NODE_STEP_SIZE} 
-                    marks={steps > 0? true: false}
-                    min={MIN_SLIDER_NODE_COUNT} 
-                    max={steps > 0? steps + SLIDER_NODE_STEP_SIZE: steps} 
-                    disabled={steps === null? true: false}
-                />
+                <StepSlider steps={steps}/>
             </div>
         </div>
     );
