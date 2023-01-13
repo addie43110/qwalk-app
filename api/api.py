07dcfebf3e4d0d9d9ca96852a860bdd6023d26f4
addie <addie@frogger.local>
2023-01-13 15:42:23 -0800

    managed to send a mpl plot figure via api call to html img but at what cost

--- api/api.py
+++ api/api.py
@@ -1,8 +1,60 @@
 import time
-from flask import Flask
+from flask import Flask, request
+import io
+
+# quantum imports
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+# importing Qiskit
+from qiskit import IBMQ, Aer, assemble, transpile
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from math import pi,log
+import matplotlib as mpl
+# import basic plot tools
+from qiskit.visualization import plot_histogram
+from qiskit.quantum_info import Statevector
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/time', methods={'GET'})
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/api/create_graphs', methods=['POST'])
+def create_graphs():
+    data = request.get_json()
+    print(data)
+    return data
+
+@app.route('/api/get_graph_test', methods=['GET'])
+def get_graph_test():
+    '''qc = QuantumCircuit(4)
+    qc.append(increment(4), [0,1,2,3])
+    qc.measure_all()
+
+    circuit_drawing = qc.decompose().draw(output='latex_source')'''
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+# QUANTUM HELPERS
+def increment(n_adder):
+    qc = QuantumCircuit(n_adder)
+    for i in range(n_adder-2):
+        qc.mcx(list(range(i+1, n_adder)),i)
+    qc.cx(n_adder-1, n_adder-2)
+    qc.x(n_adder-1)
+
+    U_inc = qc.to_gate()
+    U_inc.name = "U$_{inc}$"
+    return U_inc
--- src/component/js/GraphDisplay.js
+++ src/component/js/GraphDisplay.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import classes from "../css/GraphDisplay.module.css"
 import StepSlider from "./StepSlider";
 
-const GraphDisplay=(props)=> {
+const GraphDisplay=(props)=>{
     const {steps} = props;
+    
+    const [currentTime, setCurrentTime] = useState(0);
+    const [graph, setGraph] = useState();
+
+    /*useEffect(() => {
+        fetch('/api/get_graph_test')
+            .then(res => res.text())
+            .then(html => {
+                var parser = new DOMParser();
+                var doc = parser.parseFromString(html, "text/html");
+                var docArticle = doc.querySelector('.test').innerHTML;
+                console.log(html);
+                
+        })
+        .catch(function(err) {  
+            console.log('Failed to fetch page: ', err);  
+        });
+    }, []);*/
 
     return (
         <div className={classes["display-container"]}>
             <div className={classes.display}>
-
+                <div className="test"></div>
+                <img src="/api/get_graph_test" alt="plot"></img>
             </div>
             <div className={classes["slider-container"]}>
                 <StepSlider steps={steps}/>
