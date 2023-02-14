a97e372bcedeea030c39b02b459145c32cec6c83
Jose Ossorio <joseossorio@hotmail.com>
2023-02-13 19:16:31 -0800

    add decoding for images, move fetch image to a separate function and add loading spinner

--- src/component/js/GraphDisplay.js
+++ src/component/js/GraphDisplay.js
@@ -1,37 +1,69 @@
 import React, {useState, useEffect} from "react";
 import classes from "../css/GraphDisplay.module.css"
 import StepSlider from "./StepSlider";
+import LoadingIcons from 'react-loading-icons'
 
 const GraphDisplay=(props)=>{
     const {steps} = props;
-    
-    const [currentTime, setCurrentTime] = useState(0);
-    const [graph, setGraph] = useState();
 
-    /*useEffect(() => {
-        fetch('/api/get_graph_test')
-            .then(res => res.text())
-            .then(html => {
-                var parser = new DOMParser();
-                var doc = parser.parseFromString(html, "text/html");
-                var docArticle = doc.querySelector('.test').innerHTML;
-                console.log(html);
-                
+    const [currentTime, setCurrentTime] = useState(0);
+    const [graph, setGraph] = useState("");
+    const [loadingGraph, setLoadingGraph] = useState(false);
+
+    const decodeImage = async (res) => {
+        return new Promise(resolve => {
+            const reader = res.body.getReader();
+            resolve(new ReadableStream({
+                start(controller) {
+                    return pump();
+                    function pump() {
+                        return reader.read().then(({ done, value }) => {
+                            if (done) {
+                                controller.close();
+                                return;
+                            }
+                            controller.enqueue(value);
+                            return pump();
+                        });
+                    }
+                }
+            }))
         })
-        .catch(function(err) {  
-            console.log('Failed to fetch page: ', err);  
-        });
-    }, []);*/
+            .then((stream) => new Response(stream))
+            .then((response) => response.blob())
+            .then((blob) => URL.createObjectURL(blob))
+    }
+    const fetchGraph = () => {
+        setLoadingGraph(true);
+        fetch('http://localhost:8000/api/get_qw_test')
+            .then(res => decodeImage(res))
+            .then((url) => {
+                setGraph(url);
+                setLoadingGraph(false);
+            })
+            .catch(function(err) {
+                setLoadingGraph(false);
+                console.log('Failed to fetch page: ', err);
+            });
+    }
+
+    useEffect(() => {
+        fetchGraph();
+    }, []);
 
     return (
         <div className={classes["display-container"]}>
-            <div className={classes.display}>
-                <div className="test"></div>
-                <img src="/api/get_qw_test" alt="plot"></img>
-            </div> 
-            <div className={classes["slider-container"]}>
-                <StepSlider steps={steps}/>
-            </div>
+            {!loadingGraph ? <LoadingIcons.Grid fill={'#3880ff'} /> :
+                <>
+                    <div className={classes.display}>
+                        <div className="test"></div>
+                        <img src={graph} alt="plot"></img>
+                    </div>
+                    <div className={classes["slider-container"]}>
+                        <StepSlider steps={steps}/>
+                    </div>
+                </>
+            }
         </div>
     );
 }
